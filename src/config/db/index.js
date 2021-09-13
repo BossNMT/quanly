24823e848c9eff2fc9e25a50b4804bb8dc7e0922
BossNMT <checkpass2k3@gmail.com>
2021-09-13 16:57:00 +0700

    test mongoDB

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 async function connect() {
     
     try {
-        await mongoose.connect(process.env.DB_URL);
+        await mongoose.connect('mongodb+srv://nmt-quanly:ngominhthuan@bookservice.pysu7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
         console.log('Connect Thanh Cong');
     } catch (error) {
         console.log('Connect That Bai');
